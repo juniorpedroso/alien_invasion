bd638129bd53e279f248acd4d9d05f9c927ed185
Junior Pedroso <sintasebemagora@gmail.com>
2021-11-20 18:02:07 -0300

    Atualização

--- settings.py
+++ settings.py
@@ -13,3 +13,9 @@ class Settings():
 
         # Configurações da espaçonave
         self.ship_speed_factor = 1.5
+
+        # configurações dos porjéteis
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
