218168e3c2ad3a747d4fbe58cf2f6c669309377b
Junior Pedroso <sintasebemagora@gmail.com>
2021-11-22 16:19:19 -0300

    Atualização

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,7 +20,7 @@ def run_game():
     aliens = Group()
 
     # Cria a frota de alienígenas
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # Inicia o laço principal do jogo
     while True:
--- game_functions.py
+++ game_functions.py
@@ -89,21 +89,34 @@ def get_number_aliens_x(ai_settings, alien_width):
     return number_aliens_x
 
 
-def create_alien(ai_settings, screen, aliens, alien_number):
+def get_number_rows(ai_settings, ship_height, alien_height):
+    """[Determina o número de linhas com alienígenas que cabem na tela]"""
+    available_space_y = (ai_settings.screen_height -
+                         (3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
+
+
+def create_alien(ai_settings, screen, aliens, alien_number, row_number):
     """[Cria um alienígena e o posiciona na linha]"""
     alien = Alien(ai_settings, screen)
     alien_width = alien.rect.width
     alien.x = alien_width + 2 * alien_width * alien_number
     alien.rect.x = alien.x
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
     aliens.add(alien)
 
 
-def create_fleet(ai_settings, screen, aliens):
+def create_fleet(ai_settings, screen, ship, aliens):
     """[Cria uma frota completa de alienígenas]"""
     # Cria um alienígena e calcula o número de alienígenas em uma linha
     alien = Alien(ai_settings, screen)
     number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+    number_rows = get_number_rows(ai_settings, ship.rect.height,
+                                  alien.rect.height)
 
-    # Cria a primeira linha de alienígenas
-    for alien_number in range(number_aliens_x):
-        create_alien(ai_settings, screen, aliens, alien_number)
+    # Cria a frota de alienígenas
+    for row_number in range(number_rows):
+        # Cria a primeira linha de alienígenas
+        for alien_number in range(number_aliens_x):
+            create_alien(ai_settings, screen, aliens, alien_number, row_number)
